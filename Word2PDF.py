8c91e181f790b785637ddc52b468aac29f6ffc22
kingcobraer <997818503@qq.com>
2023-04-09 22:24:55 +0800

    增加coinitialize

--- Word2PDF.py
+++ Word2PDF.py
@@ -1,6 +1,7 @@
 from win32com.client import Dispatch
 from os import walk
 import streamlit as st
+import pythoncom
 
 directory = st.text_input('请输入文件夹地址')
 if directory is None:
@@ -8,11 +9,14 @@ if directory is None:
 
 wdFormatPDF = 17
 def doc2pdf(input_file):
+    pythoncom.CoInitialize()
     word = Dispatch('Word.Application')
     doc = word.Documents.Open(input_file)
     doc.SaveAs(input_file.replace(".docx", ".pdf"), FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
+    #释放资源
+    pythoncom.CoUninitialize()
 
 if __name__ == "__main__":
     doc_files = []
@@ -20,4 +24,4 @@ if __name__ == "__main__":
         for file in filenames:
             if file.endswith(".doc") or file.endswith(".docx"):
                 doc2pdf(str(root + "\\" + file))
-'# 1 级标题'
+'# 1 级 标 题'
